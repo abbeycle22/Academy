0a062503863cc4468d2cf2f9aa55c605b58af23a
Abbey C <abbeycle22@gmail.com>
2018-07-16 19:16:51 -0700

    Insurance

---
+++ Insurance/Insurance/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insurance Approval Form");
+            Console.WriteLine();
+            Console.WriteLine("What is your age?");
+            int num1 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Have you ever had a DUI?");
+            string drink = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets have you had?");
+            int num2 = Convert.ToInt16(Console.ReadLine());
+            int age = 15;
+            string dui = "no";
+            int ticket = 3;
+            Console.WriteLine("Qualified?");
+            bool Qualified = (num1 > 15 && drink == "no" && num2 < ticket);
+            Console.WriteLine(Qualified);
+
+
+            Console.ReadLine();
+        }
+    }
+}
