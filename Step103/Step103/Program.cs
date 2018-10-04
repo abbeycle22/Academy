d7678d737e14d195f65dcd710c4560fcaf32c870
Abbey C <abbeycle22@gmail.com>
2018-10-04 12:39:18 -0700

    Checking

---
+++ Step103/Step103/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step103
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////(Number 1)
+            //string[] names = { "Lorri", "Lane", "Abbey", "Kyle", "Casi", "Kendall", "Kaidyn" };
+
+            //Console.WriteLine("Last name of the family that lives in the Pacific Northwest?");
+            //string text = Console.ReadLine();
+
+            //Console.WriteLine("Here are the members of the " + text + " family:");
+            //for (int i = 0; i < names.Length; i++)
+            //{
+            //    Console.WriteLine(names[i] + " " + text);
+            //}
+
+            //(Number 2)
+
+            //int[] childrensAges = { 1, 3, 3, 9, 14, 16, 18 };
+
+            //for (; ; )
+            //{
+            //    Console.WriteLine("Too Many");
+
+            //}
+
+            //(Number 3)
+            //int[] childrensAges = { 1, 3, 3, 9, 14, 16, 18 };
+
+            //for (int i = 0; i < childrensAges.Length; i++)
+            //    Console.WriteLine("The Children are " + childrensAges[i]);
+
+            //(Number 4)
+
+            //int[] childrensAges = { 1, 3, 3, 9, 14, 16, 18 };
+
+            //for (int i = 0; i < childrensAges.Length; i++)
+            //{
+            //    if (i < 10)
+            //    {
+            //        Console.WriteLine("The Children are " + childrensAges[i]);
+            //    }
+            //    else
+            //    break;
+            //}
+
+            //(Number 5)
+            int[] grade = { 78, 88, 99, 45, 69 };
+
+            for (int i = 0; i < grade.Length; i++) 
+            {
+                if (i <= 80)
+                {
+                    Console.WriteLine("Passing grades are " + grade[i]);
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ sample/sample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Bran", "Rob", "Eddard", "Sonsa", "Riccon", "Arya", "Catlin" };
+
+            Console.WriteLine("Last name of the family that lives at Winterfell?");
+            string text = Console.ReadLine();
+
+            Console.WriteLine("Here are the members of the " + text + " family:");
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i] + " " + text);
+            }
+            Console.ReadLine();
+        }
+    }
+}
