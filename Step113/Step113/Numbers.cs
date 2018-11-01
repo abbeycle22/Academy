4dfc9ae725885ffcadb510472895e08aac77b1c1
Abbey C <abbeycle22@gmail.com>
2018-11-01 13:22:09 -0700

    HEEEELLLLLP!!!

---
+++ Step113/Step113/Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step113
+{
+   
+    public class Numbers
+    {
+        public int numOne;
+        public int numTwo;
+
+        public void Answer(int x , int y)
+        {
+        Console.WriteLine("Plsease select a number: ");
+        int numOne = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please select another number: ");
+        int numTwo = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("The first number you chose multiplied by 2 is : " + 2 * numOne);
+        Console.WriteLine("The second number you chose is: " + numTwo);
+
+        Console.ReadLine();
+        }
+
+    }
+
+}
---
+++ Step113/Step113/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step113
+{
+    class Program
+    {
+        //public static int numOne { get; private set; }
+        //public static int numTwo { get; private set; }
+
+        public static void Main(string[] args)
+        {
+
+            Numbers newOne = new Numbers();
+            int numOne = newOne.numOne;
+            int numTwo = newOne.numTwo;
+
+            Console.WriteLine(Numbers.Answer(numOne , numTwo));
+
+        }
+
+        //public static void Main()
+        //{
+        //    Numbers newOne = new Numbers();
+        //    int numOne = newOne.numOne;
+        //    int numTwo = newOne.numTwo;
+
+        //    Console.WriteLine("This Method does this: " + Numbers.Answer());
+        //}
+    }
+}
