82b854cfe55fb507d5d7186a755acef89f272868
Abbey C <abbeycle22@gmail.com>
2018-11-01 20:36:52 -0700

    Finished

---
+++ Step 114/Step 114/Math.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_114
+{
+    public class Math
+    {
+        internal int numThree;
+
+        public void Answer(int numOne)
+        {
+
+            Console.WriteLine("The answer to the problem is: " + (3 + numOne));
+
+        }
+
+        public void Money(decimal numTwo)
+        {
+            Console.WriteLine("The answer to the problem is: " + (3 * numTwo));
+            
+
+        }
+
+        public void Change(int x)
+        {
+            Console.WriteLine("Choose a number: ");
+            int numThree = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The answer this time is: " + (45 * numThree));
+
+        }
+    }
+}
---
+++ Step 114/Step 114/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_114
+{
+    class Program
+    {
+        static void Main()
+        {
+            Math doIt = new Math();
+            int numOne = 12;
+            decimal numTwo = .5m;
+            int numThree = doIt.numThree;
+
+            doIt.Answer(numOne);
+            doIt.Money(numTwo);
+            doIt.Change(numThree);
+
+            Console.ReadLine();
+        }
+    }
+}
