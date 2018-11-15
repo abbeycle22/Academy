8cac6dc51ae91a7e35b1278ac9986a4d10bdb6ed
Abbey C <abbeycle22@gmail.com>
2018-11-14 20:33:35 -0800

    done

---
+++ Step 126/Step 126/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_126
+{
+    class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: ");
+            base.SayName();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("To come later!");
+        }
+
+        //public void Quit(Person person)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
