0b7ea544c2de3b8c2da4a49ba22a3934f578a363
Abbey C <abbeycle22@gmail.com>
2018-11-15 21:18:31 -0800

    done

---
+++ Step 129/Step 129/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_129
+{
+    public class Employee : Person, IQuittable
+    {
+        public int x;
+        public int y;
+
+        public Employee()
+        {
+        }
+        public Employee(int id1, int id2)
+        {
+            x = id1;
+            y = id2;
+        }
+        public void ShowXY()
+        {
+            Console.WriteLine("{0}{1}", x, y);
+        }
+        //public override bool Equals(object id)
+        //{
+        //    if ((Employee)id.x == this.x && (Employee)id.y == this.y)
+        //        return true;
+        //    else
+        //        return false;
+        //}
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public static bool operator ==(Employee id1, Employee id2)
+        {
+            return id1.Equals(id2);
+        }
+        public static bool operator !=(Employee id1, Employee id2)
+        {
+            return ! id1.Equals(id2);
+        }        
+        public override void SayName()
+        {
+            Console.WriteLine("Name: ");
+            base.SayName();
+        }
+        public void Quit()
+        {
+            Console.WriteLine("To come later!");
+        }
+
+        //public void Quit(Person person)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
---
+++ Step 129/Step 129/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_129
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Employee employee = new Employee();
+            int id1 = 5;
+            int id2 = 5;
+
+        if (id1 == id2)
+            {
+                Console.WriteLine("Not ok");
+            }
+        else
+            {
+                Console.WriteLine("ok");
+            }
+            Console.ReadLine();
+        }
+       
+    }
+
+}
