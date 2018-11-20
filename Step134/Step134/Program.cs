3cda58b4d0a66dbc5e44688816a05a3ffe78d3e5
Abbey C <abbeycle22@gmail.com>
2018-11-19 20:06:21 -0800

    done

---
+++ Step134/Step134/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step134
+{
+    class Program
+    {
+        public enum DaysOfTheWeek
+        {
+            Monday = 1,
+            Tuesday = 3,
+            Wednesday = 5,
+            Thursday = 7,
+            Friday = 9,
+            Saturday = 11,
+            Sunday = 13
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day is it?");
+            string userDay = Console.ReadLine();
+
+            DaysOfTheWeek weekDay;
+            try
+            {
+                string value = userDay;
+                weekDay = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), value);
+                Console.WriteLine(userDay + " is an actual day of the week.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+            Console.ReadLine();
+        }
+
+       
+
+        
+        
+    }
+}
