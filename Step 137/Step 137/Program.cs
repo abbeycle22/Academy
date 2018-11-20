c8827f914e19e8c00cfcb10036ab7dc49e5c2a8f
Abbey C <abbeycle22@gmail.com>
2018-11-19 20:57:39 -0800

    done

---
+++ Step 137/Step 137/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_137
+{
+    class Program
+    {       
+        struct Number
+        {
+            public decimal Amount;
+        }
+        static void Main(string[] args)
+        {
+            Number N;
+            N.Amount = 2005m;
+            Console.WriteLine("The amount is " + N.Amount);
+            Console.ReadLine();
+
+        }
+    }
+}
