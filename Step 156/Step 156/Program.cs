3b225eef1524ca75c030e9e0c9a5122b0b7256cd
Abbey C <abbeycle22@gmail.com>
2018-11-22 14:16:48 -0800

    done

---
+++ Step 156/Step 156/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Step_156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //public static DateTime Now { get; }
+            Console.WriteLine(DateTime.Now);
+            DateTime timeNow = DateTime.Now;
+            Console.WriteLine();
+            Console.WriteLine("Please enter a number.");
+            int hours = Convert.ToInt32(Console.ReadLine());
+
+            DateTime later = timeNow.AddHours(hours);
+            Console.WriteLine(later);
+            Console.ReadLine();
+        }
+    }
+}
