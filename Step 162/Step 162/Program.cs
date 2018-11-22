0860a6ac873ac09465b6fe93e16b9c09e3f627aa
Abbey C <abbeycle22@gmail.com>
2018-11-22 14:44:51 -0800

    I think

---
+++ Step 162/Step 162/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_162
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const int continents = 7;
+            var live = "North America";
+
+        }
+    }
+}
---
+++ Step 162/Step 162/World.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step_162
+{
+    public class World
+    {
+        public World(): this(3)
+        {
+
+        }
+        public World(int continents)
+        {
+
+        }
+    }
+}
